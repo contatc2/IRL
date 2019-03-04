8c36df19cdbe2a57fc04e7e2e6b32e195b1d933d
clementine contat <clementine.contat@gmail.com>
2019-03-04 18:00:12 +0000

    created match, friendinvitation and adjectives models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_140006) do
+ActiveRecord::Schema.define(version: 2019_03_04_173633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_invitations", force: :cascade do |t|
+    t.boolean "accepted"
+    t.bigint "friend_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friend_invitations_on_friend_id"
+    t.index ["user_id"], name: "index_friend_invitations_on_user_id"
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.boolean "match_one_accepted"
+    t.boolean "match_two_accepted"
+    t.bigint "helper_id"
+    t.bigint "match_one_id"
+    t.bigint "match_two_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["helper_id"], name: "index_matches_on_helper_id"
+    t.index ["match_one_id"], name: "index_matches_on_match_one_id"
+    t.index ["match_two_id"], name: "index_matches_on_match_two_id"
+  end
+
+  create_table "user_adjectives", force: :cascade do |t|
+    t.string "adjective"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_adjectives_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +58,10 @@ ActiveRecord::Schema.define(version: 2019_03_04_140006) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friend_invitations", "users"
+  add_foreign_key "friend_invitations", "users", column: "friend_id"
+  add_foreign_key "matches", "users", column: "helper_id"
+  add_foreign_key "matches", "users", column: "match_one_id"
+  add_foreign_key "matches", "users", column: "match_two_id"
+  add_foreign_key "user_adjectives", "users"
 end
---
+++ test/models/user_adjective_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserAdjectiveTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
