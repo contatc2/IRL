01979824f9cc8f1099c52ea90bb04aab1ca8a242
Paul Tetaz <paul.tetaz@yahoo.fr>
2019-03-04 18:14:00 +0000

    update database, new.html.erb, edit.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_140006) do
+ActiveRecord::Schema.define(version: 2019_03_04_171304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2019_03_04_140006) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "location"
+    t.date "date_of_birth"
+    t.string "picture"
+    t.string "facebook_profile"
+    t.string "twitter_profile"
+    t.string "linkedin_profile"
+    t.boolean "available"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
