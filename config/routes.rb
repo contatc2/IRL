e2d6f8b91b963035223be59a5ef8927c2b979e7b
clementine contat <clementine.contat@gmail.com>
2019-03-05 14:52:21 +0000

    added invitation request

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   root to: 'pages#home'
-  resources :users, only: %i[index profile] do
+  resources :users, only: %i[index show] do
     resources :user_adjectives, only: %i[new create]
     resources :friend_invitations, only: %i[new create]
   end
