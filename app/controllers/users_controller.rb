0e76a8a0169eaeef71117fc4c1032eec7b94daa3
Paul Tetaz <paul.tetaz@yahoo.fr>
2019-03-05 18:31:21 +0000

    You can add 3 adj and picture but no show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,23 @@ class UsersController < ApplicationController
     @friends_pending = @user.friend_invitations.where(accepted: nil)
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    user_adjectives = params[:user][:user_adjectives]
+    @useradjs = UserAdjective.new(adjective: user_adjectives[:adjective], adjective_2: user_adjectives[:adjective_2], adjective_3: user_adjectives[:adjective_3])
+    @useradjs.save
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:picture, :user_adjectives)
+    # params.require(:user).permit(:adjective, :adjective_2, :adjective_3)
+    # params.require([:user][:user_adjectives]).permit(:adjective, :adjective_2, :adjective_3)
+  end
+
+
 end
