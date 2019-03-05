c0d26469f3f1e982f7132af332c662ce7f4e4236
clementine contat <clementine.contat@gmail.com>
2019-03-05 20:11:49 +0000

    added match on profile and match creation on friend's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @invitations = FriendInvitation.where(friend: @user)
-    @accepted_invitations = FriendInvitation.where(accepted: true, user: @user)
+    @invitations = FriendInvitation.where(friend: @user, accepted: nil)
+    @accepted_invitations = FriendInvitation.where(accepted: true, user: @user).or(FriendInvitation.where(accepted: true, friend: @user))
     @pending_invitations = FriendInvitation.where(accepted: nil, user: @user)
+    @matches = Match.where(match_one: @user).or(Match.where(match_two: @user))
   end
-
 end
