db35f9c9599516a933b70281562973dbb7decd57
clementine contat <clementine.contat@gmail.com>
2019-03-12 19:31:56 +0000

    changed the facebook redirect if user has filled in the available field already

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,8 +12,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if user.persisted?
       # sign_in_and_redirect user, event: :authentication
       sign_in(user)
-      # redirect_to user_path(user), notice: 'Signed in successfully.'
-      redirect_to single_or_not_users_path, notice: 'Signed in successfully.'
+      if user.available.nil?
+        create_invitation(user) if Referral.where(friend_email: user.email).present?
+        redirect_to single_or_not_users_path, notice: 'Signed in successfully.'
+      else
+        redirect_to user_path(user), notice: 'Signed in successfully.'
+      end
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
@@ -37,7 +41,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to root_path
   end
 
-  # protected
+  protected
+
+  def create_invitation(resource)
+    @referrals = Referral.where(friend_email: resource.email)
+    @referrals.each do |referral|
+      invitation = FriendInvitation.new
+      invitation.user = referral.user
+      invitation.friend = resource
+      invitation.save
+      referral.update(accepted: true)
+    end
+  end
 
   # The path used when OmniAuth fails
   # def after_omniauth_failure_path_for(scope)
