9c2204f4535b870db4997867e2578e80395b008b
clementine contat <clementine.contat@gmail.com>
2019-03-24 21:08:02 +0000

    updated JS function to old syntax, ES6 not supported in asset pipeline

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,6 +1,6 @@
-const scrollLastMessageIntoView = () => {
+function scrollLastMessageIntoView() {
   const messages = document.querySelectorAll('.message');
-  const lastMessage = messages.last;
+  const lastMessage = messages[messages.length - 1];
 
   if (lastMessage !== undefined) {
     lastMessage.scrollIntoView();
