c53d7faa45e2335e1dcebc21043c0ca0537ae45f
clementine contat <clementine.contat@gmail.com>
2019-04-29 17:52:21 +0100

    added picture uploader for match_two_picture

--- app/models/pseudo_match.rb
+++ app/models/pseudo_match.rb
@@ -1,5 +1,5 @@
 class PseudoMatch < ApplicationRecord
-  mount_uploader :match_two_picture, PhotoUploader
+  mount_uploader :match_two_picture, MatchPictureUploader
   belongs_to :helper, class_name: 'User', foreign_key: 'helper_id'
   belongs_to :match_one, class_name: 'User', foreign_key: 'match_one_id'
 end
