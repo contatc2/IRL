f41bf2e22b08152faa32638347bf5d7a32a9d19b
clementine contat <clementine.contat@gmail.com>
2019-04-29 18:43:18 +0100

    add email validation for pseudo match

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,5 +1,5 @@
 class Referral < ApplicationRecord
   belongs_to :user
-  validates :friend_email, uniqueness: { scope: :user,
+  validates :friend_email, presence: true, uniqueness: { scope: :user,
     message: "you can only invite an email once" }
 end
