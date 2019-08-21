80d8cecb7f8cae1179107d5a997f0eb1bc965764
clementine contat <clementine.contat@gmail.com>
2019-08-21 14:02:39 +0100

    added chat-room controller and routing to suscribe to chat-room channel, chat styling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,17 @@
 class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
-    @match = Match.find(params[:match_id])
-    @message.match = @match
+    @chat_room = ChatRoom.find(params[:chat_room_id])
+    @message.chat_room = @chat_room
     @message.user = current_user
     if @message.save
       respond_to do |format|
-        format.html { redirect to match_path(@match) }
+        format.html { redirect_to chat_room_path(@chat_room) }
         format.js
       end
     else
       respond_to do |format|
-        format.html { render "match/show" }
+        format.html { render "chat_rooms/show" }
         format.js
       end
     end
